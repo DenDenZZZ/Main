d59ae345672cb01e9d93c0c4ccf28d1d5499b57b
DenDenZZZ <dendenzzzyt@gmail.com>
2025-04-13 11:43:22 +0800

    Update Blade Ball - Utility.lua

--- Blade Ball - Utility.lua
+++ Blade Ball - Utility.lua
@@ -150,7 +150,7 @@ function Utility:GetTarget()
             local Humanoid = Entity:FindFirstChildOfClass("Humanoid")
             
             if RootPart and Humanoid and Humanoid.Health > 0 then
-                local Distance = (HumanoidRootPart.Position - RootPart.Position).Magnitude
+                local Distance = Player:DistanceFromCharacter(RootPart.Position)
                 
                 if Distance < MaxDistance then
                     MaxDistance = Distance
@@ -199,7 +199,7 @@ function Utility:BallData()
     Utility.Data.Ball.dot = math.clamp(Utility.Data.Ball.direction:Dot(Utility.Data.Ball.velocity.Unit), -1, 1)
     Utility.Data.Ball.radians = math.acos(Utility.Data.Ball.dot)
     Utility.Data.Ball.angle = math.deg(Utility.Data.Ball.radians)
-    Utility.Data.Ball.distance = (Player.Character.HumanoidRootPart.Position - Utility.Data.Ball.position).Magnitude
+    Utility.Data.Ball.distance = Player:DistanceFromCharacter(Ball.Position)
     
     return unpack({
         Utility.Data.Ball.ball, Utility.Data.Ball.target, Utility.Data.Ball.from, Utility.Data.Ball.position,
